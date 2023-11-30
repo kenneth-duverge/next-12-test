fc0b2e1b5f951f23741bde06a4c0f52a0107e4ee
Kenneth Duverge <kenneth.duverge@icloud.com>
2023-11-30 06:47:07 -0500

    Add chakra-button

--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,5 +1,7 @@
 import Head from 'next/head';
 
+import { Button } from '@chakra-ui/react';
+
 export default function Home() {
   return (
     <>
@@ -9,7 +11,9 @@ export default function Home() {
         <meta name="viewport" content="width=device-width, initial-scale=1" />
         <link rel="icon" href="/favicon.ico" />
       </Head>
-      <main>Testing</main>
+      <main>
+        <Button>Click me</Button>
+      </main>
     </>
   );
 }
