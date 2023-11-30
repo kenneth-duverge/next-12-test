60491656842dec4ecdf23c28f91a5e7f4675778f
Kenneth Duverge <kenneth.duverge@icloud.com>
2023-11-30 06:44:46 -0500

    Use chakra theme and remove globals

--- src/pages/_app.tsx
+++ src/pages/_app.tsx
@@ -1,7 +1,5 @@
-import '@/styles/globals.css';
 import type { AppProps } from 'next/app';
-import { ChakraProvider } from '@chakra-ui/react';
-import { theme } from '@vimeo-ux/theme';
+import { ChakraProvider, theme } from '@chakra-ui/react';
 
 export default function App({ Component, pageProps }: AppProps) {
   return (
